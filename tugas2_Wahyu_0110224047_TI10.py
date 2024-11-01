2f0e801cb54daafcb87055b6c85bcb2a9445bfb8
wahyoe <145647812+bgwahyoe@users.noreply.github.com>
2024-11-01 09:50:55 +0700

    Add files via upload

---
+++ tugas2_Wahyu_0110224047_TI10.py
@@ -0,0 +1,18 @@
+print("Wahyu Ahmad Yassin")
+print("0110224047")
+print("24TI10")
+print("Pak Jaya")
+
+#print("-- Harap Menginput dengan benar --")
+
+#nama = str(input("Nama :"))
+#nim = int(input("Nim :"))
+#rombel = str(input("Rombel :"))
+#asdos = str(input("Asdos :"))
+
+#print("-- Hasil Inputan --")
+
+#print("Nama :", nama)
+#print("NIM :", nim)
+#print("Rombel :", rombel)
+#print("Asdos :", asdos)
